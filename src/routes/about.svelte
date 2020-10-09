a0946f1583973b6c3b3c94ced5d07b15ba66c3e0
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-10-09 18:34:22 +0200

    Init

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	type Options = "op1" | "op2";
+
+	let opt: Options = "op2";
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<p>{opt}</p>
