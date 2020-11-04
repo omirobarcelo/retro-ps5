92499165aeff3e1e192ae7496963125fdfdb80a1
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-11-04 16:35:05 +0100

    Added json suffix to avoid clashing with frontend route, added send 404 on game not found

--- src/routes/game/[id].json.ts
+++ src/routes/game/[id].json.ts
@@ -4,5 +4,10 @@ import models from '../../models';
 export async function get(req: Request, res: Response, next: NextFunction) {
   const { id } = req.params;
   const game = await models.Game.findById(id);
+  if (!game) {
+    return res.status(404).send({
+      message: 'Game Not Found!'
+    });
+  }
   return res.send(game);
 }
