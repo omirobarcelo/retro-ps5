7d95d2dba021e01fd465c05670e3192547e3bf5d
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-11-05 16:53:42 +0100

    Moved calcPercentage to a utils file

---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+import type { GameListItem } from './data/interfaces/game-list-item.interface';
+
+/**
+ * Returns text percentage for positive or negative votes, with 2 decimals
+ * @param game 
+ * @param type 
+ */
+export const calcPercentage = (game: GameListItem, type: 'positive' | 'negative'): string => {
+  const votes = type === 'positive' ? game.positiveVotes : game.negativeVotes;
+  const perc = (votes * 100) / (game.positiveVotes + game.negativeVotes);
+  return `${perc.toFixed(2)}%`;
+};
