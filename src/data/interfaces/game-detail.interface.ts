00c3244e6a03107f0f9ad20730be5d52ce39637a
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-11-05 16:47:19 +0100

    Corrected type for comments in Game Detail interface

--- src/data/interfaces/game-detail.interface.ts
+++ src/data/interfaces/game-detail.interface.ts
@@ -2,7 +2,7 @@ import type { CommentKind } from '../types/comment-kind.type';
 import type { GameListItem } from './game-list-item.interface';
 
 export interface GameDetail extends GameListItem {
-  comments: { kind: CommentKind; text: string; date: Date };
+  comments: { kind: CommentKind; text: string; date: Date }[];
   locked: boolean;
   accepted: boolean;
   createdAt: Date;
