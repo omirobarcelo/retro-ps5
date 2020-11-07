305bedf3d0aa529c064aa4cfcbcb10c4486569ff
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-11-08 00:28:20 +0100

    Added Contact model

---
+++ src/models/contact.model.ts
@@ -0,0 +1,45 @@
+import { Document, model, Model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    read: { type: Boolean, default: false },
+    dealt: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+);
+
+contactSchema.statics.findUnread = async function (): Promise<IContactDocument[]> {
+  return (this as IContactModel).find({ read: false }).sort({ createdAt: 0 });
+};
+
+contactSchema.statics.findUndealt = async function (): Promise<IContactDocument[]> {
+  return (this as IContactModel).find({ dealt: false }).sort({ createdAt: 0 });
+};
+
+interface IContactDocument extends Document {
+  // Fields
+  title: string;
+  description: string;
+  read: boolean;
+  dealt: boolean;
+
+  // Methods
+}
+
+interface IContactModel extends Model<IContactDocument> {
+  // Statics
+  findUnread(): Promise<IContactDocument[]>;
+  findUndealt(): Promise<IContactDocument[]>;
+}
+
+const Contact = model<IContactDocument, IContactModel>('Contact', contactSchema);
+
+export default Contact;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,12 @@
 import { connect } from 'mongoose';
+import Contact from './contact.model';
 import Game from './game.model';
 
 const connectDb = (db: string) => {
   return connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 };
 
-const models = { Game };
+const models = { Game, Contact };
 
 export { connectDb };
 
