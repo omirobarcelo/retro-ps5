3a14d2c22881fbd85b57fa0b9d8ddd3f21f75ed3
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-11-10 00:05:36 +0100

    Redirect to list when focus on searchbar, disable on create

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import { goto } from '@sapper/app';
   import { onMount, onDestroy } from 'svelte';
   import { fromEvent, Subscription } from 'rxjs';
   import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
   import { search } from '../stores/search.store';
 
-  // TODO disable input if segment is create, or move to list if segment is game or contact
   export let segment: string;
-  // TODO if segment is create, update search to be empty
+  $: if (segment === 'create') {
+    search.update('');
+    // If navigating directly to create, this executes before the onMount
+    if (searchElem) {
+      searchElem.value = '';
+    }
+  }
 
   let searchElem: HTMLInputElement;
 
@@ -26,6 +32,11 @@
   });
 
   onDestroy(() => subscriptions.unsubscribe());
+
+  async function onSearchBarClick() {
+    await goto('/');
+    searchElem.focus();
+  }
 </script>
 
 <style lang="postcss">
@@ -45,10 +56,12 @@
   </div>
   <input
     bind:this={searchElem}
+    on:click={onSearchBarClick}
     class="appearance-none border w-1/2 py-1 px-2 text-gray-700 focus:outline-none focus:shadow-outline"
     id="search"
     type="search"
     placeholder="Search..."
+    disabled={segment === 'create'}
   />
   <div class="flex-grow flex items-center w-auto ml-6">
     <div class="flex-grow">
